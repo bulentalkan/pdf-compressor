3d1f10ea69b7415de76bc2e315ada158845f4a1f
bulentalkan <bulentalkan2k@gmail.com>
2026-08-31 03:55:44 +0300

    fix preset mapping: max compression should use screen not prepress

---
+++ engine/strategies.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+from options import CompressionOptions
+
+class CompressionStrategy(ABC):
+    @abstractmethod
+    def get_options(self) -> CompressionOptions:
+        ...     #  ellipsis (...) is used to indicate that the method is abstract and should be implemented by subclasses
+        
+class FastStrategy(CompressionStrategy):
+    def get_options(self) -> CompressionOptions:
+        return CompressionOptions(gs_preset="printer") 
+
+
+class BalancedStrategy(CompressionStrategy):
+    def get_options(self) -> CompressionOptions:
+        return CompressionOptions(gs_preset="ebook")
+
+
+class MaxCompressionStrategy(CompressionStrategy):
+    def get_options(self) -> CompressionOptions:
+        return CompressionOptions(gs_preset="screen")
+
+
+if __name__ == "__main__":
+    for strategy in [FastStrategy(), BalancedStrategy(), MaxCompressionStrategy()]:
+        print(strategy.get_options())
